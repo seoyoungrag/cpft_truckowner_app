ffbbf4ae0049dd79822fffa09b5a35ff4821e5a0
서영락 <seoyoungrag@seoyeonglag-ui-MacBookPro.local>
2020-07-26 22:34:15 +0900

    아이폰 처리 추가

--- screens/Auth/PatternLock/PatternLockContianer.js
+++ screens/Auth/PatternLock/PatternLockContianer.js
@@ -14,6 +14,7 @@ import styled from "styled-components";
 import * as Animatable from "react-native-animatable";
 import constants from "../../../constants";
 import PatternLockPresenter from "./PatternLockPresenter";
+import { Ionicons } from "@expo/vector-icons";
 
 const { width, height } = Dimensions.get("window");
 
@@ -202,7 +203,7 @@ export default ({ loginSuccess }) => {
      />
      {Platform.OS === "ios" && (
       <View style={styles.backButtonContainer}>
-       <Icon
+       <Ionicons
         component={TouchableOpacity}
         onPress={onBackPress}
         name="chevron-left"
