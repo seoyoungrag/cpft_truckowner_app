3039f8447b07fac49a20c5aa88be02a5e2a625b3
jaeha <jaeha.kim@timf.co.kr>
2020-11-24 14:15:22 +0900

    통신 테스트 커밋

--- screens/DocketForm/DocketForm.js
+++ screens/DocketForm/DocketForm.js
@@ -1,9 +1,9 @@
 import React from "react";
 import {View, Text} from "react-native";
-import { createStackNavigator } from "@react-navigation/stack";
-import { FontAwesome5, Entypo } from "@expo/vector-icons";
+import {createStackNavigator} from "@react-navigation/stack";
+import {FontAwesome5, Entypo} from "@expo/vector-icons";
 import ScrollContainer from "../../components/ScrollContainer";
-import { useIsModal } from "../../ModalContext";
+import {useIsModal} from "../../ModalContext";
 
 import TsBasic from "../DocketForm/TsBasic";
 import LfBasic from "../DocketForm/LfBasic";
@@ -13,51 +13,42 @@ import DailyWork from "../DocketForm/DailyWork";
 const DocketFormStacks = createStackNavigator();
 const DocketFormNav = createStackNavigator();
 
-const DocketFormContainer = ({ navigation, route }) => {
-  return (
-      
-    <DocketFormNav.Navigator>
-
-      <DocketFormNav.Screen name="TS 기본" component={TsBasic}/>
-      <DocketFormNav.Screen name="LF 기본" component={LfBasic}/>
-      <DocketFormNav.Screen name="LF 이디야" component={LfEdiya}/>
-      <DocketFormNav.Screen name="용차" component={DailyWork}/>
-
-    </DocketFormNav.Navigator>
-    
-  )
-}
-
+const DocketFormContainer = ({navigation, route}) => {
+	return (
+		<DocketFormNav.Navigator>
+			<DocketFormNav.Screen name="TS 기본" component={TsBasic} />
+			<DocketFormNav.Screen name="LF 기본" component={LfBasic} />
+			<DocketFormNav.Screen name="LF 이디야" component={LfEdiya} />
+			<DocketFormNav.Screen name="용차" component={DailyWork} />
+		</DocketFormNav.Navigator>
+	);
+};
 
 export default ({navigation, route}) => {
-  return (
-    <>
-    <DocketFormStacks.Navigator>
-
-    <DocketFormStacks.Screen
-    name="DocketFormContainer"
-    component={DocketFormContainer}
-    options={{
-      title: "명세서",
-      headerStyle: {
-        backgroundColor: "#3e50b4",
-      },
-      headerTitleStyle: {
-        textAlign: "center",
-        marginLeft: -44,
-        paddingLeft: 0,
-        fontSize: 24,
-        color: "white",
-      },
-      headerTintColor: "white",
-      headerBackTitleVisible: false,
-      headerShown: true,
-    }}
-    >
-      
-    </DocketFormStacks.Screen>
-    </DocketFormStacks.Navigator>
-
-    </>
-  )
-}
+	return (
+		<>
+			<DocketFormStacks.Navigator>
+				<DocketFormStacks.Screen
+					name="DocketFormContainer"
+					component={DocketFormContainer}
+					options={{
+						title: "명세서",
+						headerStyle: {
+							backgroundColor: "#3e50b4",
+						},
+						headerTitleStyle: {
+							textAlign: "center",
+							marginLeft: -44,
+							paddingLeft: 0,
+							fontSize: 24,
+							color: "white",
+						},
+						headerTintColor: "white",
+						headerBackTitleVisible: false,
+						headerShown: true,
+					}}
+				></DocketFormStacks.Screen>
+			</DocketFormStacks.Navigator>
+		</>
+	);
+};
--- screens/Trans/TransCard.js
+++ screens/Trans/TransCard.js
@@ -1,86 +1,104 @@
 import React from "react";
-import { Text, View, Modal, TouchableOpacity } from "react-native";
+import {Text, View, Modal, TouchableOpacity} from "react-native";
 import * as Calc from "../../components/Calc";
 import * as rq from "react-query";
-import { useNavigation } from "@react-navigation/native";
+import {useNavigation} from "@react-navigation/native";
 import axios from "axios";
 
 export default (props) => {
-  const navigation = useNavigation();
+	const navigation = useNavigation();
 
-  const [updateCall] = rq.useMutation(
-    (obj) => {
-      axios.post("https://blueapi.teamfresh.co.kr/v2/trans/updateApproveStatus", obj);
-    },
-    {
-      onSuccess: async (data, mutationVariables) => {}
-    }
-  );
+	const [updateCall] = rq.useMutation(
+		(obj) => {
+			axios.post("https://blueapi.teamfresh.co.kr/v2/trans/updateApproveStatus", obj);
+		},
+		{
+			onSuccess: async (data, mutationVariables) => {},
+		}
+	);
 
-  const updateCheckYn = React.useCallback(() => {
-    const obj = {
-      matchingCode: props.data.matchingCode,
-      targetMonth: props.data.targetMonth,
-      excelSeq: props.data.excelSeq
-    };
-    updateCall(obj);
-  }, [props]);
+	const updateCheckYn = React.useCallback(() => {
+		const obj = {
+			matchingCode: props.data.matchingCode,
+			targetMonth: props.data.targetMonth,
+			excelSeq: props.data.excelSeq,
+		};
+		updateCall(obj);
+	}, [props]);
 
-  return (
-    <>
-      <View style={{ padding: 15 }}>
-        <View style={{ borderWidth: 1, borderColor: "black", padding: 15, borderRadius: 10, backgroundColor: "white" }}>
-          <View>
-            <View style={{ flexDirection: "row", marginBottom: 5, justifyContent: "space-between" }}>
-              <Text style={{ fontWeight: "bold", fontSize: 20 }}>{props.data.carrierNm}</Text>
-              <Text style={{ fontSize: 20 }}>{props.data.fixingDailyWork}</Text>
-            </View>
-            <View style={{ flexDirection: "row", justifyContent: "center" }}>
-              <Text style={{ fontWeight: "bold", fontSize: 20 }}>{props.data.owrProfsNm}</Text>
-            </View>
-            <View style={{ flexDirection: "row", justifyContent: "center" }}>
-              <Text style={{ fontWeight: "bold", fontSize: 25 }}>{Calc.regexWON(props.data.total)}</Text>
-            </View>
-          </View>
-          <View style={{ flexDirection: "row", justifyContent: "space-between", marginTop: 10 }}>
-            <View style={{ flex: 11, alignItems: "center", borderWidth: 1, borderColor: "#3e50b4", borderRadius: 5, height: 38 }}>
-              <TouchableOpacity
-                style={{ width: "100%", height: "100%", alignItems: "center" }}
-                onPress={() => {
-                  navigation.navigate("DocketForm", {
-                    screen: "DocketFormContainer",
-                    params: {
-                      screen: props.data.docketForm,
-                      params: {
-                        matchingCode: props.data.matchingCode,
-                        // truckManagerCode: props.data.truckManagerCode,
-                        targetMonth: props.data.targetMonth,
-                        excelSeq: props.data.excelSeq
-                      }
-                    }
-                  });
-                  updateCheckYn();
-                }}
-              >
-                <Text style={{ color: "#3e50b4", fontSize: 18, height: "100%", textAlignVertical: "center", fontWeight: "bold" }}>명세서</Text>
-              </TouchableOpacity>
-            </View>
-            <View style={{ flex: 1 }}></View>
-            <View style={{ flex: 11, alignItems: "center", borderWidth: 1, borderColor: "#3e50b4", borderRadius: 5, backgroundColor: "#3e50b4", height: 38 }}>
-              <TouchableOpacity
-                onPress={() =>
-                  navigation.navigate("TaxBillDetail", {
-                    screen: "TaxBillDetail"
-                  })
-                }
-                style={{ width: "100%", height: "100%", alignItems: "center" }}
-              >
-                <Text style={{ color: "white", fontSize: 18, height: "100%", textAlignVertical: "center", fontWeight: "bold" }}>세금계산서</Text>
-              </TouchableOpacity>
-            </View>
-          </View>
-        </View>
-      </View>
-    </>
-  );
+	return (
+		<>
+			<View style={{padding: 15}}>
+				<View style={{borderWidth: 1, borderColor: "black", padding: 15, borderRadius: 10, backgroundColor: "white"}}>
+					<View>
+						<View style={{flexDirection: "row", marginBottom: 5, justifyContent: "space-between"}}>
+							<Text style={{fontWeight: "bold", fontSize: 20}}>{props.data.carrierNm}</Text>
+							<Text style={{fontSize: 20}}>{props.data.fixingDailyWork}</Text>
+						</View>
+						<View style={{flexDirection: "row", justifyContent: "center"}}>
+							<Text style={{fontWeight: "bold", fontSize: 20}}>{props.data.owrProfsNm}</Text>
+						</View>
+						<View style={{flexDirection: "row", justifyContent: "center"}}>
+							<Text style={{fontWeight: "bold", fontSize: 25}}>{Calc.regexWON(props.data.total)}</Text>
+						</View>
+					</View>
+					<View style={{flexDirection: "row", justifyContent: "space-between", marginTop: 10}}>
+						<View style={{flex: 11, alignItems: "center", borderWidth: 1, borderColor: "#3e50b4", borderRadius: 5, height: 38}}>
+							<TouchableOpacity
+								style={{width: "100%", height: "100%", alignItems: "center"}}
+								onPress={() => {
+									navigation.navigate("DocketForm", {
+										screen: "DocketFormContainer",
+										params: {
+											screen: props.data.docketForm,
+											params: {
+												targetMonth: props.targetMonth,
+												driverSeq: props.data.driverSeq,
+												carrierSeq: 1,
+												taxBillSeq: props.data.taxBillSeq,
+												taxBillItemSeq: props.data.taxBillItemSeq,
+												matchingCode: props.data.matchingCode,
+												// truckManagerCode: props.data.truckManagerCode,
+												// excelSeq: props.data.excelSeq,
+											},
+										},
+									});
+									updateCheckYn();
+								}}
+							>
+								<Text style={{color: "#3e50b4", fontSize: 18, height: "100%", textAlignVertical: "center", fontWeight: "bold"}}>명세서</Text>
+							</TouchableOpacity>
+						</View>
+						<View style={{flex: 1}}></View>
+						<View style={{flex: 11, alignItems: "center", borderWidth: 1, borderColor: "#3e50b4", borderRadius: 5, backgroundColor: "#3e50b4", height: 38}}>
+							<TouchableOpacity
+								onPress={() =>
+									navigation.navigate("TaxBillDetail", {
+										screen: "TaxBillDetail",
+										params: {
+											screen: props.data.docketForm,
+											params: {
+												targetMonth: props.targetMonth,
+												driverSeq: props.data.driverSeq,
+												carrierSeq: 1,
+												carryingSeq: props.data.carryingSeq,
+												taxBillSeq: props.data.taxBillSeq,
+												taxBillItemSeq: props.data.taxBillItemSeq,
+												matchingCode: props.data.matchingCode,
+												// truckManagerCode: props.data.truckManagerCode,
+												// excelSeq: props.data.excelSeq,
+											},
+										},
+									})
+								}
+								style={{width: "100%", height: "100%", alignItems: "center"}}
+							>
+								<Text style={{color: "white", fontSize: 18, height: "100%", textAlignVertical: "center", fontWeight: "bold"}}>세금계산서</Text>
+							</TouchableOpacity>
+						</View>
+					</View>
+				</View>
+			</View>
+		</>
+	);
 };
